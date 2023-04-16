337e57998264936dd83165604a208d1f6c3d44f8
Leo Riche <leo.riche@edu.devinci.fr>
2023-04-16 20:28:36 +0200

    Sidebar

---
+++ settings.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manga Mind Settings</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/sidebar.js" defer></script>
+</head>
+<body>
+    <header>
+        <img class="ProfilPicture" src="./images/PPLeo.png" alt="Photo de profil Clubs">
+        <h1>Manga Mind</h1>
+        <h5 class="MenuBurger">Menu JS</h5>
+    </header>
+    <section class="Sidebar Slide">
+        <a href="home.html"><h3 class="ElementSidebar">Accueil</h3></a>
+        <h3 class="ElementSidebar">Recherche</h3>
+        <h3 class="ElementSidebar">Messages</h3>
+        <a href="profil.html"><h3 class="ElementSidebar">Profil</h3></a>
+        <a href="settings.html"><h3 class="ElementSidebar">Paramètres</h3></a>
+        <h3 class="ElementSidebar" class="LogoutSidebar">Se déconnecter</h3>
+    </section>
+</body>
+</html>
